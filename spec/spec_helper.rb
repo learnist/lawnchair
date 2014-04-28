def96e609a5fb17b9a4ca6e6974a9cceebcd2bcb
Jake Frautschi <linus@learnist.com>
2014-04-28 15:51:55 -0700

    remove old 'spec' includes - using latest rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'lawnchair'
-require 'spec'
-require 'spec/autorun'
 require 'active_record_extension'
 Spec::Runner.configure do |config|
   config.before(:all)   { Lawnchair.connectdb(Redis.new(:db => 11)) }
@@ -10,7 +8,7 @@ Spec::Runner.configure do |config|
     Lawnchair.flushdb
     abstract_store = Lawnchair::StorageEngine::Abstract
     abstract_store.data_store.keys.each {|k| abstract_store.data_store.delete(k)}
-    
+
     in_process = Lawnchair::StorageEngine::InProcess
     in_process.data_store.keys.each {|k| in_process.data_store.delete(k)}
   end
